1c1e4287b85a0aed625467aa7b79706c2cb3666a
zerosnacks <zerosnacks@protonmail.com>
2022-05-06 18:28:48 +0200

    move core interfaces into interfaces/core

--- src/test/CEther.t.sol
+++ src/test/CEther.t.sol
@@ -4,10 +4,10 @@ pragma solidity 0.8.13;
 import {Test} from "forge-std/Test.sol";
 
 // Interfaces
-import {IIFuseFeeDistributor} from "../interfaces/IIFuseFeeDistributor.sol";
-import {IComptrollerInterface} from "../interfaces/IComptrollerInterface.sol";
-import {ICEther} from "../interfaces/ICEther.sol";
-import {IInterestRateModel} from "../interfaces/IInterestRateModel.sol";
+import {IIFuseFeeDistributor} from "../interfaces/core/IIFuseFeeDistributor.sol";
+import {IComptrollerInterface} from "../interfaces/core/IComptrollerInterface.sol";
+import {ICEther} from "../interfaces/core/ICEther.sol";
+import {IInterestRateModel} from "../interfaces/core/IInterestRateModel.sol";
 
 contract CEtherTest is Test {
     IIFuseFeeDistributor internal constant fuseAdmin =
