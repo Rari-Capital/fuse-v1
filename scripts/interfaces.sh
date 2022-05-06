43a816085a7e76ce0608d19ddedbd22a1173647c
zerosnacks <zerosnacks@protonmail.com>
2022-05-06 16:40:17 +0200

    prefix interfaces with I

--- scripts/interfaces.sh
+++ scripts/interfaces.sh
@@ -4,18 +4,18 @@ shopt -s extglob
 
 # Run from root
 
-rm -rf generated/abi
+# rm -rf generated/abi
 rm -rf generated/interfaces
 
-mkdir -p generated/abi
+# mkdir -p generated/abi
 mkdir -p generated/interfaces
 
 for f in src/core/*.sol; do
     name=${f//+(*\/|.*)}
 
-    forge inspect ${name} abi > generated/abi/${name}.json
-    cast interface generated/abi/${name}.json > generated/interfaces/${name}.sol
-    sed -i "s/interface Interface/interface I${name}/g" generated/interfaces/${name}.sol
+    # forge inspect ${name} abi > generated/abi/${name}.json
+    cast interface generated/abi/${name}.json > generated/interfaces/I${name}.sol
+    sed -i "s/interface Interface/interface I${name}/g" generated/interfaces/I${name}.sol
 done;
 
-# make lint-fix
+make lint-fix
--- src/test/CEther.t.sol
+++ src/test/CEther.t.sol
@@ -4,12 +4,22 @@ pragma solidity 0.8.13;
 import {Test} from "forge-std/Test.sol";
 
 // Interfaces
-import {ICEther} from "../interfaces/CEther.sol";
+import {ICEther} from "../interfaces/ICEther.sol";
+import {IComptrollerInterface} from "../interfaces/IComptrollerInterface.sol";
+import {IInterestRateModel} from "../interfaces/IInterestRateModel.sol";
 
-contract CEther is Test {
+contract CEtherTest is Test {
+    IComptrollerInterface public comptrollerInterface;
+    IInterestRateModel public interestRateModel;
     ICEther public cEther;
 
     function setUp() public {
+        comptrollerInterface = IComptrollerInterface(
+            deployCode("ComptrollerInterface.sol:ComptrollerInterface")
+        );
+        interestRateModel = IInterestRateModel(
+            deployCode("InterestRateModel.sol:InterestRateModel")
+        );
         cEther = ICEther(deployCode("CEther.sol:CEther"));
 
         vm.label(address(cEther), "CEther");
