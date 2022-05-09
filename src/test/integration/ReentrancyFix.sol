67a7154fd294100a369b5943ae077511308b7d19
zerosnacks <zerosnacks@protonmail.com>
2022-05-09 22:26:07 +0200

    fix syntax error

--- src/test/integration/ReentrancyFix.sol
+++ src/test/integration/ReentrancyFix.sol
@@ -32,27 +32,27 @@ contract ReentrancyFix is Test {
         );
     }
 
-//     function testShouldDisallowReentrancyWithUpgradedCEtherDelegate() public {
-//         // Deploy new CEtherDelegate (using deployCode)
-//         // Get pool 8 admin
-//         // Impersonate pool 8 admin
-//         // Get FuseFeeDistributor owner
-//         // Impersonate FuseFeeDistributor owner
-//         // Call FuseFeeDistributor._editCEtherDelegateWhitelist
-//         // Call CEther._setImplementationSafe
+    //     function testShouldDisallowReentrancyWithUpgradedCEtherDelegate() public {
+    //         // Deploy new CEtherDelegate (using deployCode)
+    //         // Get pool 8 admin
+    //         // Impersonate pool 8 admin
+    //         // Get FuseFeeDistributor owner
+    //         // Impersonate FuseFeeDistributor owner
+    //         // Call FuseFeeDistributor._editCEtherDelegateWhitelist
+    //         // Call CEther._setImplementationSafe
 
-//         // Impersonate exploiter
-//         vm.startPrank(exploiter);
+    //         // Impersonate exploiter
+    //         vm.startPrank(exploiter);
 
-//         // Exploit should revert
-//         vm.expectRevert();
+    //         // Exploit should revert
+    //         vm.expectRevert();
 
-//         ErrorHandling._functionCall(
-//             address(0x32075bAd9050d4767018084F0Cb87b3182D36C45),
-//             "0x34930754000000000000000000000000c54172e34046c1653d1920d40333dd358c7a1af4000000000000000000000000bb025d470162cc5ea24daf7d4566064ee7f5f1110000000000000000000000006b175474e89094c44da98b954eedeac495271d0f0000000000000000000000000000000000000000004a723dc6b40b8a9a00000000000000000000000000000000000000000000000000000000000000000000a00000000000000000000000000000000000000000000000000000000000000003000000000000000000000000d8553552f8868c1ef160eedf031cf0bcf96869450000000000000000000000007e9ce3caa9910cc048590801e64174957ed41d43000000000000000000000000647a36d421183a0a9fa62717a64b664a24e469c7",
-//             "CALL_FAIL"
-//         );
-//     }
-// }
+    //         ErrorHandling._functionCall(
+    //             address(0x32075bAd9050d4767018084F0Cb87b3182D36C45),
+    //             "0x34930754000000000000000000000000c54172e34046c1653d1920d40333dd358c7a1af4000000000000000000000000bb025d470162cc5ea24daf7d4566064ee7f5f1110000000000000000000000006b175474e89094c44da98b954eedeac495271d0f0000000000000000000000000000000000000000004a723dc6b40b8a9a00000000000000000000000000000000000000000000000000000000000000000000a00000000000000000000000000000000000000000000000000000000000000003000000000000000000000000d8553552f8868c1ef160eedf031cf0bcf96869450000000000000000000000007e9ce3caa9910cc048590801e64174957ed41d43000000000000000000000000647a36d421183a0a9fa62717a64b664a24e469c7",
+    //             "CALL_FAIL"
+    //         );
+    //     }
+}
 
 // https://github.com/foundry-rs/forge-std/issues/22
