24807e9c058b8ec9b1c96faceeefdccf0881c991
zerosnacks <zerosnacks@protonmail.com>
2022-05-10 16:33:20 +0200

    add fox token and ichi vault

--- src/test/integration/pools/FusePool79.t.sol
+++ src/test/integration/pools/FusePool79.t.sol
@@ -5,6 +5,9 @@ import {Test} from "forge-std/Test.sol";
 import {console2} from "forge-std/console2.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
 
+// External interfaces
+import {IICHIVault} from "../../interfaces/external/ichi/IICHIVault.sol";
+
 // Interfaces
 import {ICErc20} from "../../interfaces/core/ICErc20.sol";
 import {IUnitroller} from "../../interfaces/core/IUnitroller.sol";
@@ -13,6 +16,9 @@ import {IComptroller} from "../../interfaces/core/IComptroller.sol";
 
 // Pool 79: Fox and Frens
 // https://etherscan.io/address/0x41c7B863FdDa5eb7CF8D8f748B136d10d7AEC631 (pool)
+// https://app.rari.capital/token/0x779f9bad1f4b1ef5198ad9361dbf3791f9e0d596 (token)
+
+// Specifically we are interested in Uniswap V2 oneFOX-FOX LP
 
 contract FusePool79 is Test {
     address user = 0xB290f2F3FAd4E540D0550985951Cdad2711ac34A;
@@ -26,28 +32,38 @@ contract FusePool79 is Test {
     IComptroller internal constant comptroller =
         IComptroller(0x613Ea1dC49E83eAd05db49DcFcF57b22Fb5510bD);
 
-    // Uniswap V3 oneFOX-FOX: https://etherscan.io/address/0x202a4a99ab3833528e2dda446730e95cb004093e#readContract
-    address internal constant oneFoxFoxLpToken =
-        0x202a4a99aB3833528e2ddA446730E95CB004093E;
-
     // oneFOX token: https://etherscan.io/token/0x03352d267951e96c6f7235037c5dfd2ab1466232
     ERC20 internal constant oneFoxToken =
         ERC20(0x03352D267951E96c6F7235037C5DFD2AB1466232);
+
+    // FOX token: https://etherscan.io/address/0xc770eefad204b5180df6a14ee197d99d808ee52d
+    ERC20 internal constant foxToken =
+        ERC20(0xc770EEfAd204B5180dF6a14Ee197D99d808ee52d);
+
+    // ICHIVault oneFOX-FOX: https://etherscan.io/address/0x202a4a99ab3833528e2dda446730e95cb004093e#readContract
+    // https://angel.ichi.org/
+    IICHIVault internal constant ICHIVaultoneFOXFOXLPToken =
+        IICHIVault(0x779F9BAd1f4B1Ef5198AD9361DBf3791F9e0D596);
 
     function setUp() public {
         vm.label(address(pool), "pool");
         vm.label(address(fuseAdmin), "fuseAdmin");
         vm.label(address(comptroller), "comptroller");
-        vm.label(address(oneFoxFoxLpToken), "oneFoxFoxLpToken");
         vm.label(address(oneFoxToken), "oneFoxToken");
+        vm.label(address(foxToken), "foxToken");
+        vm.label(
+            address(ICHIVaultoneFOXFOXLPToken),
+            "ICHIVaultoneFOXFOXLPToken"
+        );
     }
 
     function testPool79() public {
         vm.startPrank(user);
 
-        deal(address(oneFoxToken), user, 1e18);
+        deal(address(oneFoxToken), user, 100e18);
+        deal(address(foxToken), user, 100e18);
 
-        console2.log(oneFoxToken.balanceOf(address(user)));
+        ICHIVaultoneFOXFOXLPToken.deposit(100e18, 100e18, user);
 
         assertTrue(true);
     }
---
+++ src/test/interfaces/external/ichi/IICHIVault.sol
@@ -0,0 +1,197 @@
+pragma solidity ^0.8.10;
+
+interface IICHIVault {
+    event Affiliate(address indexed sender, address affiliate);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+    event DeployICHIVault(
+        address indexed sender,
+        address indexed pool,
+        bool allowToken0,
+        bool allowToken1,
+        address owner,
+        uint256 twapPeriod
+    );
+    event Deposit(
+        address indexed sender,
+        address indexed to,
+        uint256 shares,
+        uint256 amount0,
+        uint256 amount1
+    );
+    event DepositMax(
+        address indexed sender,
+        uint256 deposit0Max,
+        uint256 deposit1Max
+    );
+    event Hysteresis(address indexed sender, uint256 hysteresis);
+    event MaxTotalSupply(address indexed sender, uint256 maxTotalSupply);
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
+    event Rebalance(
+        int24 tick,
+        uint256 totalAmount0,
+        uint256 totalAmount1,
+        uint256 feeAmount0,
+        uint256 feeAmount1,
+        uint256 totalSupply
+    );
+    event SetTwapPeriod(address sender, uint32 newTwapPeriod);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Withdraw(
+        address indexed sender,
+        address indexed to,
+        uint256 shares,
+        uint256 amount0,
+        uint256 amount1
+    );
+
+    function PRECISION() external view returns (uint256);
+
+    function affiliate() external view returns (address);
+
+    function allowToken0() external view returns (bool);
+
+    function allowToken1() external view returns (bool);
+
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function baseLower() external view returns (int24);
+
+    function baseUpper() external view returns (int24);
+
+    function currentTick() external view returns (int24 tick);
+
+    function decimals() external view returns (uint8);
+
+    function decreaseAllowance(address spender, uint256 subtractedValue)
+        external
+        returns (bool);
+
+    function deposit(
+        uint256 deposit0,
+        uint256 deposit1,
+        address to
+    ) external returns (uint256 shares);
+
+    function deposit0Max() external view returns (uint256);
+
+    function deposit1Max() external view returns (uint256);
+
+    function fee() external view returns (uint24);
+
+    function getBasePosition()
+        external
+        view
+        returns (
+            uint128 liquidity,
+            uint256 amount0,
+            uint256 amount1
+        );
+
+    function getLimitPosition()
+        external
+        view
+        returns (
+            uint128 liquidity,
+            uint256 amount0,
+            uint256 amount1
+        );
+
+    function getTotalAmounts()
+        external
+        view
+        returns (uint256 total0, uint256 total1);
+
+    function hysteresis() external view returns (uint256);
+
+    function ichiVaultFactory() external view returns (address);
+
+    function increaseAllowance(address spender, uint256 addedValue)
+        external
+        returns (bool);
+
+    function limitLower() external view returns (int24);
+
+    function limitUpper() external view returns (int24);
+
+    function maxTotalSupply() external view returns (uint256);
+
+    function name() external view returns (string memory);
+
+    function owner() external view returns (address);
+
+    function pool() external view returns (address);
+
+    function rebalance(
+        int24 _baseLower,
+        int24 _baseUpper,
+        int24 _limitLower,
+        int24 _limitUpper,
+        int256 swapQuantity
+    ) external;
+
+    function renounceOwnership() external;
+
+    function setAffiliate(address _affiliate) external;
+
+    function setDepositMax(uint256 _deposit0Max, uint256 _deposit1Max) external;
+
+    function setHysteresis(uint256 _hysteresis) external;
+
+    function setMaxTotalSupply(uint256 _maxTotalSupply) external;
+
+    function setTwapPeriod(uint32 newTwapPeriod) external;
+
+    function symbol() external view returns (string memory);
+
+    function tickSpacing() external view returns (int24);
+
+    function token0() external view returns (address);
+
+    function token1() external view returns (address);
+
+    function totalSupply() external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    function transferOwnership(address newOwner) external;
+
+    function twapPeriod() external view returns (uint32);
+
+    function uniswapV3MintCallback(
+        uint256 amount0,
+        uint256 amount1,
+        bytes memory data
+    ) external;
+
+    function uniswapV3SwapCallback(
+        int256 amount0Delta,
+        int256 amount1Delta,
+        bytes memory data
+    ) external;
+
+    function withdraw(uint256 shares, address to)
+        external
+        returns (uint256 amount0, uint256 amount1);
+}
