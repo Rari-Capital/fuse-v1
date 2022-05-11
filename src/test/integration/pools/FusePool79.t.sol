1c292d0955675cd5e5587993bd634fdb845e62fa
zerosnacks <zerosnacks@protonmail.com>
2022-05-11 17:25:51 +0200

    figuring out why borrow is paused

--- src/test/integration/pools/FusePool79.t.sol
+++ src/test/integration/pools/FusePool79.t.sol
@@ -9,10 +9,10 @@ import {ERC20} from "solmate/tokens/ERC20.sol";
 import {IICHIVault} from "../../interfaces/external/ichi/IICHIVault.sol";
 
 // Interfaces
+import {IFuseFeeDistributor} from "../../interfaces/IFuseFeeDistributor.sol";
 import {IFuseSafeLiquidator} from "../../interfaces/IFuseSafeLiquidator.sol";
 import {ICErc20} from "../../interfaces/core/ICErc20.sol";
 import {IUnitroller} from "../../interfaces/core/IUnitroller.sol";
-import {IIFuseFeeDistributor} from "../../interfaces/core/IIFuseFeeDistributor.sol";
 import {IComptroller} from "../../interfaces/core/IComptroller.sol";
 import {IOneFoxLiquidator} from "../../interfaces/liquidators/IOneFoxLiquidator.sol";
 
@@ -25,8 +25,11 @@ contract FusePool79 is Test {
     IUnitroller internal constant pool =
         IUnitroller(0x613Ea1dC49E83eAd05db49DcFcF57b22Fb5510bD);
 
-    IIFuseFeeDistributor internal constant fuseAdmin =
-        IIFuseFeeDistributor(0x90A48D5CF7343B08dA12E067680B4C6dbfE551Be);
+    IFuseFeeDistributor internal constant globalFuseAdmin =
+        IFuseFeeDistributor(0xa731585ab05fC9f83555cf9Bff8F58ee94e18F85);
+
+    IFuseFeeDistributor internal constant fuseAdmin =
+        IFuseFeeDistributor(0x90A48D5CF7343B08dA12E067680B4C6dbfE551Be);
 
     IFuseSafeLiquidator internal constant fuseSafeLiquidator =
         IFuseSafeLiquidator(0xF0f3a1494aE00B5350535b7777abB2f499fC13d4);
@@ -37,13 +40,12 @@ contract FusePool79 is Test {
     ERC20 internal constant USDCToken =
         ERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
 
+    ERC20 internal constant FEIToken =
+        ERC20(0x956F47F50A910163D8BF957Cf5846D573E7f87CA);
+
     // oneFOX token: https://etherscan.io/token/0x03352d267951e96c6f7235037c5dfd2ab1466232
     ERC20 internal constant oneFoxToken =
         ERC20(0x03352D267951E96c6F7235037C5DFD2AB1466232);
-
-    // FOX token: https://etherscan.io/address/0xc770eefad204b5180df6a14ee197d99d808ee52d
-    // ERC20 internal constant foxToken =
-    //     ERC20(0xc770EEfAd204B5180dF6a14Ee197D99d808ee52d);
 
     // ICHIVaultLP oneFOX-FOX: https://etherscan.io/address/0x779F9BAd1f4B1Ef5198AD9361DBf3791F9e0D596
     // https://angel.ichi.org/
@@ -55,8 +57,6 @@ contract FusePool79 is Test {
 
     IOneFoxLiquidator public oneFoxLiquidator;
 
-    // IOneFoxLiquidator oneFoxLiquidator = new OneFoxLiquidator();
-
     function setUp() public {
         oneFoxLiquidator = IOneFoxLiquidator(
             deployCode("OneFoxLiquidator.sol:OneFoxLiquidator")
@@ -66,13 +66,27 @@ contract FusePool79 is Test {
         vm.label(address(fuseAdmin), "fuseAdmin");
         vm.label(address(fuseSafeLiquidator), "fuseSafeLiquidator");
         vm.label(address(comptroller), "comptroller");
+        vm.label(address(USDCToken), "USDCToken");
+        vm.label(address(FEIToken), "FEIToken");
         vm.label(address(oneFoxToken), "oneFoxToken");
-        // vm.label(address(foxToken), "foxToken");
         vm.label(address(ICHIVaultLP), "ICHIVaultLP");
         vm.label(address(cICHIVaultLP), "cICHIVaultLP");
     }
 
     function testPool79() public {
+        // NOTE: temporary: unpause the global pools
+        // address owner = 0x5eA4A9a7592683bF0Bc187d6Da706c6c4770976F;
+        // vm.startPrank(owner);
+        // globalFuseAdmin._setPoolLimits(0, type(uint256).max, type(uint256).max);
+        // vm.stopPrank();
+
+        // NOTE: currently reject _setPoolLimits?
+        // NOTE: temporary: unpause the local pool
+        // address owner = 0x90A48D5CF7343B08dA12E067680B4C6dbfE551Be;
+        // vm.startPrank(owner);
+        // fuseAdmin._setPoolLimits(0, type(uint256).max, type(uint256).max);
+        // vm.stopPrank();
+
         vm.startPrank(user);
 
         deal(address(oneFoxToken), user, 100e18);
@@ -87,12 +101,13 @@ contract FusePool79 is Test {
 
         ICHIVaultLP.approve(address(cICHIVaultLP), type(uint256).max);
 
-        deal(address(cICHIVaultLP), user, 100e18);
         uint256 cICHIVaultLPShares = cICHIVaultLP.mint(100e18);
         require(cICHIVaultLPShares == 0, "Mint failed");
 
+        console2.log(ICHIVaultLP.balanceOf(user));
+
         // Currently the borrow is paused
-        cICHIVaultLP.borrow(100e18);
+        // cICHIVaultLP.borrow(10e18);
 
         // fuseSafeLiquidator.redeemCustomCollateral(underlyingCollateral, underlyingCollateralSeized, strategy, strategyData);
     }
