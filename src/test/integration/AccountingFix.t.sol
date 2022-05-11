12d509bdbbb94c00e24764dd2bbea2c8f1c98792
zerosnacks <zerosnacks@protonmail.com>
2022-05-11 15:29:41 +0200

    add more test code

--- src/test/integration/AccountingFix.t.sol
+++ src/test/integration/AccountingFix.t.sol
@@ -1,104 +1,113 @@
 pragma solidity 0.8.13;
 
-// // Vendor
-// import {Test} from "forge-std/Test.sol";
+// Vendor
+import {Test} from "forge-std/Test.sol";
+import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
 
-// // Interfaces
-// import {ICEtherDelegate} from "../interfaces/core/ICEtherDelegate.sol";
-// import {ICEtherDelegateTempExploitAccounting} from "../interfaces/core/ICEtherDelegateTempExploitAccounting.sol";
-// import {IComptroller} from "../interfaces/core/IComptroller.sol";
-// import {IFuseFeeDistributor} from "../interfaces/IFuseFeeDistributor.sol";
+// Interfaces
+import {ICEtherDelegate} from "../interfaces/core/ICEtherDelegate.sol";
+import {ICEtherDelegateTempExploitAccounting} from "../interfaces/core/ICEtherDelegateTempExploitAccounting.sol";
+import {IComptroller} from "../interfaces/core/IComptroller.sol";
+import {IFuseFeeDistributor} from "../interfaces/IFuseFeeDistributor.sol";
 
-// contract AccountingFix is Test {
-//     ICEtherDelegateTempExploitAccounting
-//         internal cEtherDelegateTempExploitAccounting;
+contract AccountingFix is Test {
+    using FixedPointMathLib for uint256;
 
-//     ICEtherDelegate internal cEtherDelegate;
+    ICEtherDelegateTempExploitAccounting
+        internal cEtherDelegateTempExploitAccounting;
 
-//     function setUp() public {}
+    ICEtherDelegate internal cEtherDelegate;
 
-//     function testShouldMergeAttackersSupplyAndBorrowBalances() public {
-//         // Deploy new CEtherDelegateTempExploitAccounting
-//         cEtherDelegateTempExploitAccounting = ICEtherDelegateTempExploitAccounting(
-//             deployCode(
-//                 "CEtherDelegateTempExploitAccounting.sol:CEtherDelegateTempExploitAccounting"
-//             )
-//         );
+    function setUp() public {}
 
-//         // Deploy new CEtherDelegate
-//         cEtherDelegate = ICEtherDelegate(
-//             deployCode("CEtherDelegate.sol:CEtherDelegate")
-//         );
+    function testShouldMergeAttackersSupplyAndBorrowBalances() public {
+        // Deploy new CEtherDelegateTempExploitAccounting
+        cEtherDelegateTempExploitAccounting = ICEtherDelegateTempExploitAccounting(
+            deployCode(
+                "CEtherDelegateTempExploitAccounting.sol:CEtherDelegateTempExploitAccounting"
+            )
+        );
 
-//         // Get pool 8 admin
-//         IComptroller comptroller = IComptroller(
-//             0xc54172e34046c1653d1920d40333Dd358c7a1aF4
-//         );
-//         address comptrollerAdmin = comptroller.admin();
-//         vm.label(address(comptrollerAdmin), "comptrollerAdmin");
+        // Deploy new CEtherDelegate
+        cEtherDelegate = ICEtherDelegate(
+            deployCode("CEtherDelegate.sol:CEtherDelegate")
+        );
 
-//         // Impersonate admin of pool 8
-//         vm.startPrank(comptrollerAdmin);
+        // Get pool 8 admin
+        IComptroller comptroller = IComptroller(
+            0xc54172e34046c1653d1920d40333Dd358c7a1aF4
+        );
+        address comptrollerAdmin = comptroller.admin();
+        vm.label(address(comptrollerAdmin), "comptrollerAdmin");
 
-//         // Get FuseFeeDistributor owner
-//         IFuseFeeDistributor fuseFeeDistributor = IFuseFeeDistributor(
-//             address(0xa731585ab05fC9f83555cf9Bff8F58ee94e18F85)
-//         );
-//         vm.label(address(fuseFeeDistributor), "fuseFeeDistributor");
+        // Impersonate admin of pool 8
+        vm.startPrank(comptrollerAdmin);
 
-//         address fuseFeeDistributorOwner = fuseFeeDistributor.owner();
-//         vm.label(address(fuseFeeDistributorOwner), "fuseFeeDistributorOwner");
+        // Get FuseFeeDistributor owner
+        IFuseFeeDistributor fuseFeeDistributor = IFuseFeeDistributor(
+            address(0xa731585ab05fC9f83555cf9Bff8F58ee94e18F85)
+        );
+        vm.label(address(fuseFeeDistributor), "fuseFeeDistributor");
 
-//         // Impersonate FuseFeeDistributor owner
-//         vm.stopPrank();
-//         vm.startPrank(fuseFeeDistributorOwner);
+        address fuseFeeDistributorOwner = fuseFeeDistributor.owner();
+        vm.label(address(fuseFeeDistributorOwner), "fuseFeeDistributorOwner");
 
-//         // Call FuseFeeDistributor._editCEtherDelegateWhitelist
-//         // fuseFeeDistributor._edit
+        // Impersonate FuseFeeDistributor owner
+        vm.stopPrank();
+        vm.startPrank(fuseFeeDistributorOwner);
 
-//         assertTrue(true);
-//     }
-// }
+        // Call FuseFeeDistributor._editCEtherDelegateWhitelist
+        address[] memory oldImplementations = new address[](2);
+        oldImplementations[0] = 0xd77E28A1b9a9cFe1fc2EEE70E391C05d25853cbF;
+        oldImplementations[1] = address(cEtherDelegateTempExploitAccounting);
+
+        address[] memory newImplementations = new address[](2);
+        newImplementations[0] = address(cEtherDelegateTempExploitAccounting);
+        newImplementations[1] = address(cEtherDelegate);
+
+        bool[] memory allowResign = new bool[](2);
+        allowResign[0] = false;
+        allowResign[1] = false;
+
+        bool[] memory statuses = new bool[](2);
+        statuses[0] = true;
+        statuses[1] = true;
+
+        fuseFeeDistributor._editCEtherDelegateWhitelist(
+            oldImplementations,
+            newImplementations,
+            allowResign,
+            statuses
+        );
+
+        // Get attacker's initial balancess
+        uint256 exchangeRateStored = cEtherDelegate.exchangeRateStored();
+        uint256 account1SupplySharesInitial = cEtherDelegate.balanceOf(
+            0x32075bAd9050d4767018084F0Cb87b3182D36C45
+        );
+        uint256 account1UnderlyingSupplyBalanceInitial = account1SupplySharesInitial
+                .mulDivDown(exchangeRateStored, 10**18);
+        uint256 account1UnderlyingBorrowBalanceInitial = cEtherDelegate
+            .borrowBalanceStored(0x32075bAd9050d4767018084F0Cb87b3182D36C45);
+        uint256 account2UnderlyingSupplyBalanceInitial = cEtherDelegate
+            .balanceOf(0x3686657208883d016971c7395eDaeD73c107383E)
+            .mulDivDown(exchangeRateStored, 10**18);
+        uint256 account2UnderlyingBorrowBalanceInitial = cEtherDelegate
+            .borrowBalanceStored(0x3686657208883d016971c7395eDaeD73c107383E);
+
+        assertGt(account1UnderlyingSupplyBalanceInitial, 0);
+        assertEq(account1UnderlyingBorrowBalanceInitial, 0);
+        assertEq(account2UnderlyingSupplyBalanceInitial, 0);
+        assertGt(account2UnderlyingBorrowBalanceInitial, 0);
+
+        assertTrue(true);
+    }
+}
 
 // // const { ethers } = require("hardhat");
 // // const { expect } = require("chai");
 
 // // describe("CEtherDelegateTempExploitAccounting", function () {
-// //   it("Should merge the attacker's supply and borrow balances", async function () {
-// //     // Enable using 0 gas price
-// //     await hre.network.provider.send("hardhat_setNextBlockBaseFeePerGas", ["0x0"]);
-
-// //     // Deploy new CEtherDelegateTempExploitAccounting
-// //     const CEtherDelegateTempExploitAccounting = await ethers.getContractFactory("CEtherDelegateTempExploitAccounting");
-// //     var cEtherDelegateTempExploitAccounting = await CEtherDelegateTempExploitAccounting.deploy();
-
-// //     // Deploy new CEtherDelegate
-// //     const CEtherDelegate = await ethers.getContractFactory("CEtherDelegate");
-// //     var cEtherDelegate = await CEtherDelegate.deploy();
-
-// //     // Get pool 8 admin
-// //     const Comptroller = await ethers.getContractFactory("Comptroller");
-// //     var comptroller = Comptroller.attach("0xc54172e34046c1653d1920d40333dd358c7a1af4");
-// //     var comptrollerAdmin = await comptroller.admin();
-
-// //     // Impersonate admin of pool 8
-// //     await hre.network.provider.request({
-// //         method: "hardhat_impersonateAccount",
-// //         params: [comptrollerAdmin],
-// //     });
-
-// //     // Get FuseFeeDistributor owner
-// //     var ffd = new ethers.Contract("0xa731585ab05fC9f83555cf9Bff8F58ee94e18F85", [{"inputs":[{"internalType":"address[]","name":"oldImplementations","type":"address[]"},{"internalType":"address[]","name":"newImplementations","type":"address[]"},{"internalType":"bool[]","name":"allowResign","type":"bool[]"},{"internalType":"bool[]","name":"statuses","type":"bool[]"}],"name":"_editCEtherDelegateWhitelist","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"}], ethers.provider);
-// //     var ffdOwner = await ffd.owner();
-
-// //     // Impersonate FuseFeeDistributor owner
-// //     await hre.network.provider.request({
-// //         method: "hardhat_impersonateAccount",
-// //         params: [ffdOwner],
-// //     });
-
-// //     // Call FuseFeeDistributor._editCEtherDelegateWhitelist
-// //     await ffd.connect(await ethers.getSigner(ffdOwner))._editCEtherDelegateWhitelist(["0xd77e28a1b9a9cfe1fc2eee70e391c05d25853cbf", cEtherDelegateTempExploitAccounting.address], [cEtherDelegateTempExploitAccounting.address, cEtherDelegate.address], [false, false], [true, true], { gasPrice: "0" });
 
 // //     // Get attacker's initial balances
 // //     var cEther = CEtherDelegate.attach("0xbB025D470162CC5eA24daF7d4566064EE7f5F111");
