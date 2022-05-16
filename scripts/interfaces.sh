8b1dfd225c4084ff414473a834d9b561e17d3f75
zerosnacks <zerosnacks@protonmail.com>
2022-05-16 17:46:14 +0200

    add utils abi and interface

--- scripts/interfaces.sh
+++ scripts/interfaces.sh
@@ -8,7 +8,7 @@ rm -rf generated
 
 mkdir -p generated/abi generated/interfaces
 
-for f in src/oracles/*.sol; do
+for f in src/utils/*.sol; do
     name=${f//+(*\/|.*)}
 
     forge inspect ${name} abi > generated/abi/${name}.json
