d681b270098181cf90291e1992a3aa5037d08994
zerosnacks <zerosnacks@protonmail.com>
2022-05-17 14:18:11 +0200

    add recommended files to repo

---
+++ src/plugins/CErc20PluginDelegate.sol
@@ -0,0 +1,103 @@
+pragma solidity ^0.5.16;
+pragma experimental ABIEncoderV2;
+
+import "../core/CErc20Delegate.sol";
+import "./EIP20Interface.sol";
+
+/**
+ * @title Rari's CErc20Plugin's Contract
+ * @notice CToken which outsources token logic to a plugin
+ * @author Joey Santoro
+ *
+ * CErc20PluginDelegate deposits and withdraws from a plugin conract
+ * It is also capable of delegating reward functionality to a PluginRewardsDistributor
+ */
+contract CErc20PluginDelegate is CErc20Delegate {
+    /**
+     * @notice Plugin address
+     */
+    IERC4626 public plugin;
+
+    uint256 public constant PRECISION = 1e18;
+
+    /**
+     * @notice Delegate interface to become the implementation
+     * @param data The encoded arguments for becoming
+     */
+    function _becomeImplementation(bytes calldata data) external {
+        require(msg.sender == address(this) || hasAdminRights());
+
+        address _plugin = abi.decode(data, (address));
+
+        require(_plugin != address(0), "0");
+
+        if (
+            address(plugin) != address(0) &&
+            plugin.balanceOf(address(this)) != 0
+        ) {
+            plugin.redeem(
+                plugin.balanceOf(address(this)),
+                address(this),
+                address(this)
+            );
+        }
+
+        plugin = IERC4626(_plugin);
+
+        EIP20Interface(underlying).approve(_plugin, uint256(-1));
+
+        uint256 amount = EIP20Interface(underlying).balanceOf(address(this));
+        if (amount != 0) {
+            deposit(amount);
+        }
+    }
+
+    /*** CToken Overrides ***/
+
+    /*** Safe Token ***/
+
+    /**
+     * @notice Gets balance of the plugin in terms of the underlying
+     * @return The quantity of underlying tokens owned by this contract
+     */
+    function getCashPrior() internal view returns (uint256) {
+        return plugin.previewRedeem(plugin.balanceOf(address(this)));
+    }
+
+    /**
+     * @notice Transfer the underlying to the cToken and trigger a deposit
+     * @param from Address to transfer funds from
+     * @param amount Amount of underlying to transfer
+     * @return The actual amount that is transferred
+     */
+    function doTransferIn(address from, uint256 amount)
+        internal
+        returns (uint256)
+    {
+        // Perform the EIP-20 transfer in
+        require(
+            EIP20Interface(underlying).transferFrom(
+                from,
+                address(this),
+                amount
+            ),
+            "send"
+        );
+
+        deposit(amount);
+        return amount;
+    }
+
+    function deposit(uint256 amount) internal {
+        plugin.deposit(amount, address(this));
+    }
+
+    /**
+     * @notice Transfer the underlying from plugin to destination
+     * @param to Address to transfer funds to
+     * @param amount Amount of underlying to transfer
+     */
+    function doTransferOut(address payable to, uint256 amount) internal {
+        plugin.withdraw(amount, to, address(this));
+    }
+}
---
+++ src/plugins/IERC4626.sol
@@ -0,0 +1,152 @@
+pragma solidity ^0.5.16;
+pragma experimental ABIEncoderV2;
+
+import "./EIP20Interface.sol";
+
+contract IERC4626 is EIP20Interface {
+    /*///////////////////////////////////////////////////////////////
+                                Events
+    //////////////////////////////////////////////////////////////*/
+
+    event Deposit(address indexed from, address indexed to, uint256 value);
+
+    event Withdraw(address indexed from, address indexed to, uint256 value);
+
+    /*///////////////////////////////////////////////////////////////
+                            Mutable Functions
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+      @notice Deposit a specific amount of underlying tokens.
+      @param amount The amount of the underlying token to deposit.
+      @param to The address to receive shares corresponding to the deposit
+      @return shares The shares in the vault credited to `to`
+    */
+    function deposit(uint256 amount, address to)
+        public
+        returns (uint256 shares);
+
+    /**
+      @notice Mint an exact amount of shares for a variable amount of underlying tokens.
+      @param shares The amount of vault shares to mint.
+      @param to The address to receive shares corresponding to the mint.
+      @return amount The amount of the underlying tokens deposited from the mint call.
+    */
+    function mint(uint256 shares, address to) public returns (uint256 amount);
+
+    /**
+      @notice Withdraw a specific amount of underlying tokens.
+      @param amount The amount of the underlying token to withdraw.
+      @param to The address to receive underlying corresponding to the withdrawal.
+      @param from The address to burn shares from corresponding to the withdrawal.
+      @return shares The shares in the vault burned from sender
+    */
+    function withdraw(
+        uint256 amount,
+        address to,
+        address from
+    ) public returns (uint256 shares);
+
+    /**
+      @notice Redeem a specific amount of shares for underlying tokens.
+      @param from The address to burn shares from corresponding to the redemption.
+      @param to The address to receive underlying corresponding to the redemption.
+      @param shares The amount of shares to redeem.
+      @return amount The underlying amount transferred to `to`.
+    */
+    function redeem(
+        uint256 shares,
+        address to,
+        address from
+    ) public returns (uint256 amount);
+
+    /*///////////////////////////////////////////////////////////////
+                            View Functions
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+      @notice The underlying token the Vault accepts.
+      @return the ERC20 underlying implementation address.
+    */
+    function asset() public view returns (EIP20Interface);
+
+    /**
+      @notice Returns a user's Vault balance in underlying tokens.
+      @param user The user to get the underlying balance of.
+      @return amount The user's Vault balance in underlying tokens.
+    */
+    function assetsOf(address user) public view returns (uint256 amount);
+
+    /**
+      @notice Calculates the total amount of underlying tokens the Vault manages.
+      @return The total amount of underlying tokens the Vault manages.
+    */
+    function totalAssets() public view returns (uint256);
+
+    /**
+      @notice Returns the value in underlying terms of one vault token.
+     */
+    function exchangeRate() public view returns (uint256);
+
+    /**
+      @notice Returns the amount of vault tokens that would be obtained if depositing a given amount of underlying tokens in a `deposit` call.
+      @param amount the input amount of underlying tokens
+      @return shares the corresponding amount of shares out from a deposit call with `amount` in
+     */
+    function previewDeposit(uint256 amount)
+        public
+        view
+        returns (uint256 shares);
+
+    /**
+      @notice Returns the amount of underlying tokens that would be deposited if minting a given amount of shares in a `mint` call.
+      @param shares the amount of shares from a mint call.
+      @return amount the amount of underlying tokens corresponding to the mint call
+     */
+    function previewMint(uint256 shares) public view returns (uint256 amount);
+
+    /**
+      @notice Returns the amount of vault tokens that would be burned if withdrawing a given amount of underlying tokens in a `withdraw` call.
+      @param amount the input amount of underlying tokens
+      @return shares the corresponding amount of shares out from a withdraw call with `amount` in
+     */
+    function previewWithdraw(uint256 amount)
+        public
+        view
+        returns (uint256 shares);
+
+    /**
+      @notice Returns the amount of underlying tokens that would be obtained if redeeming a given amount of shares in a `redeem` call.
+      @param shares the amount of shares from a redeem call.
+      @return amount the amount of underlying tokens corresponding to the redeem call
+     */
+    function previewRedeem(uint256 shares) public view returns (uint256 amount);
+
+    /**
+      @notice Returns the max deposit amount for a recipient
+      @param to the deposit recipient
+      @return amount the max input amount for deposit for a user
+    */
+    function maxDeposit(address to) public view returns (uint256 amount);
+
+    /**
+      @notice Returns the max mint shares for a recipient
+      @param to the mint recipient
+      @return shares the max shares for mint for a user
+    */
+    function maxMint(address to) public view returns (uint256 shares);
+
+    /**
+      @notice Returns the max withdraw amount for a user
+      @param from the withdraw source
+      @return amount the max amount out for withdraw for a user
+    */
+    function maxWithdraw(address from) public view returns (uint256 amount);
+
+    /**
+      @notice Returns the max redeem shares for a user
+      @param from the redeem source
+      @return shares the max shares out for redeem for a user
+    */
+    function maxRedeem(address from) public view returns (uint256 shares);
+}
