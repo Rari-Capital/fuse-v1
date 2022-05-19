25045251dc9e2d78b44a9294c685f7bb3b03ff9d
sriyantra <85423375+sriyantra@users.noreply.github.com>
2022-05-19 13:21:44 -0700

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -82,7 +82,7 @@ export default {
         version: '0.5.17',
         settings: {
           optimizer: {
-            enabled: foundry.default?.optimizer || true,
+            enabled: true,
             runs: 200
           }
         }
@@ -91,7 +91,7 @@ export default {
         version: '0.6.12',
         settings: {
           optimizer: {
-            enabled: foundry.default?.optimizer || true,
+            enabled: true,
             runs: 200
           }
         }
@@ -100,7 +100,7 @@ export default {
         version: '0.8.10',
         settings: {
           optimizer: {
-            enabled: foundry.default?.optimizer || true,
+            enabled: true,
             runs: 200
           }
         }
