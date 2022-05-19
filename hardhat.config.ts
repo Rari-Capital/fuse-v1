55d2fa8e2349625dd1eb7dbb02805490f9b2fe12
sriyantra <85423375+sriyantra@users.noreply.github.com>
2022-05-19 12:28:12 -0700

    create hardhat.config.ts

---
+++ hardhat.config.ts
@@ -0,0 +1,106 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+import { readFileSync } from 'fs';
+import * as toml from 'toml';
+import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-etherscan';
+import 'hardhat-gas-reporter';
+import 'solidity-coverage';
+import 'hardhat-contract-sizer';
+import { HardhatUserConfig, subtask } from 'hardhat/config';
+import { TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS } from 'hardhat/builtin-tasks/task-names';
+
+// default values here to avoid failures when running hardhat
+const RINKEBY_RPC = process.env.RINKEBY_RPC || '1'.repeat(32);
+const ETH_RPC_URL = process.env.ETH_RPC_URL;
+const ARBITRUM_RPC_URL = process.env.ARBITRUM_RPC_URL;
+const ETH_PRIVATE_KEY = process.env.ETH_PRIVATE_KEY || '1'.repeat(64);
+const SOLC_DEFAULT = '0.8.10';
+
+// try forge config
+let foundry: any;
+try {
+  foundry = toml.parse(readFileSync('./foundry.toml').toString());
+  foundry.default.solc = foundry.default['solc-version']
+    ? foundry.default['solc-version']
+    : SOLC_DEFAULT;
+} catch (error) {
+  foundry = {
+    default: {
+      solc: SOLC_DEFAULT,
+    }
+  }
+}
+
+// prune forge style tests from hardhat paths
+subtask(TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS)
+  .setAction(async (_, __, runSuper) => {
+    const paths = await runSuper();
+    return paths.filter((p: string) => !p.endsWith('.t.sol'));
+  });
+
+export default {
+  paths: {
+    cache: 'cache-hardhat',
+    sources: './src',
+    tests: './integration',
+  },
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: { chainId: 1337 },
+    rinkeby: {
+      url: RINKEBY_RPC,
+      accounts: [ETH_PRIVATE_KEY],
+    },
+    mainnet: {
+      url: ETH_RPC_URL,
+      accounts: [ETH_PRIVATE_KEY]
+    },
+    arbitrum : {
+      url: ARBITRUM_RPC_URL
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: '0.5.17',
+        settings: {
+          optimizer: {
+            enabled: foundry.default?.optimizer || true,
+            runs: 200
+          }
+        }
+      },
+      {
+        version: '0.6.12',
+        settings: {
+          optimizer: {
+            enabled: foundry.default?.optimizer || true,
+            runs: 200
+          }
+        }
+      },
+      {
+        version: '0.8.10',
+        settings: {
+          optimizer: {
+            enabled: foundry.default?.optimizer || true,
+            runs: 200
+          }
+        }
+      }
+    ],
+  },
+  gasReporter: {
+    currency: 'USD',
+    gasPrice: 77,
+    excludeContracts: ['src/test'],
+    // API key for CoinMarketCap. https://pro.coinmarketcap.com/signup
+    coinmarketcap: process.env.CMC_KEY ?? '',
+  },
+  etherscan: {
+    // API key for Etherscan. https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+} as HardhatUserConfig;
