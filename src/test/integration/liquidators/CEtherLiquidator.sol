0eb9d2ad9b12dc50ab58d2b7309adc3f9435302b
zerosnacks <zerosnacks@protonmail.com>
2022-06-01 18:22:51 +0200

    add redeem, fails

--- src/test/integration/liquidators/CEtherLiquidator.sol
+++ src/test/integration/liquidators/CEtherLiquidator.sol
@@ -5,13 +5,16 @@ import "forge-std/Test.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
 
 // Interfaces
+import {FuseFeeDistributor} from "../../interfaces/IFuseFeeDistributor.sol";
+import {Comptroller} from "../../interfaces/core/IComptroller.sol";
 import {CErc20} from "../../interfaces/core/ICErc20.sol";
+import {CEther} from "../../interfaces/core/ICEther.sol";
 import {CEtherLiquidator} from "../../interfaces/liquidators/ICEtherLiquidator.sol";
 
 // Fixtures
 import {FuseFixture} from "../../fixtures/FuseFixture.sol";
 
-// Pool 8
+// Pool 8 is a special case, uses a custom FuseAdmin
 // Pool 6
 // Pool 18
 // Pool 127
@@ -26,36 +29,35 @@ import {FuseFixture} from "../../fixtures/FuseFixture.sol";
 
 /// @notice Redeems seized Compound/Cream/Fuse CEther cTokens for underlying ETH for use as a step in a liquidation.
 contract CEtherLiquidatorTest is Test, FuseFixture {
+    Comptroller internal poolComptroller;
+    FuseFeeDistributor internal poolFuseAdmin;
     CEtherLiquidator internal liquidator;
 
     // General
-    address user = 0xB290f2F3FAd4E540D0550985951Cdad2711ac34A;
-
-    // Token
-    ERC20 underlyingToken = ERC20(0x67B66C99D3Eb37Fa76Aa3Ed1ff33E8e39F0b9c7A);
+    address userAddress = 0xB290f2F3FAd4E540D0550985951Cdad2711ac34A;
 
     function setUp() public virtual override {
         super.setUp();
 
-        vm.startPrank(user);
+        CEther cf6ETH = CEther(0xF6551C22276b9Bf62FaD09f6bD6Cad0264b89789);
+        CErc20 cf6FEI = CErc20(0x185Ab80A77D362447415a5B347D7CD86ecaCC87C);
 
-        deal(address(underlyingToken), user, 100e18);
-        assertGe(underlyingToken.balanceOf(user), 100e18);
+        vm.startPrank(userAddress);
 
-        // Unpause pool
+        vm.deal(userAddress, 1 << 128);
+        assertGe(userAddress.balance, 1 << 128);
 
-        // Deposit into pool 8
-
-        CErc20 cETH8f = CErc20(0xbB025D470162CC5eA24daF7d4566064EE7f5F111);
-        require(cETH8f.mint(100e18) == 0, "mint failed");
+        cf6ETH.mint{value: 10 ether}();
+        uint256 cf6ETHBalance = cf6ETH.balanceOf(userAddress);
+        assertGt(cf6ETHBalance, 0);
 
         liquidator = CEtherLiquidator(
             deployCode("artifacts/CEtherLiquidator.sol/CEtherLiquidator.json")
         );
 
-        // liquidator.redeem(inputToken, inputAmount, strategyData);
+        cf6FEI.borrow(100e18);
 
-        //
+        liquidator.redeem(address(cf6ETH), cf6ETHBalance, "");
 
         console2.log(address(liquidator));
     }
