eb64883f7d95ee72b55edd5df81268e0aedc39f1
zerosnacks <zerosnacks@protonmail.com>
2022-06-08 14:56:25 +0200

    refactor

--- sdk/Fuse/index.ts
+++ sdk/Fuse/index.ts
@@ -5,12 +5,7 @@ import { providers, Contract } from "ethers";
 import { ChainID, isSupportedChainId } from "../utilities/networks";
 
 // Data
-import { ADDRESSES } from "./data/addresses";
-import {
-  FUSE_CONTRACTS,
-  CORE_CONTRACTS,
-  ORACLE_CONTRACTS,
-} from "./data/contracts";
+import { getContracts } from "./data/contracts";
 
 export class Fuse {
   provider: providers.JsonRpcProvider;
@@ -18,44 +13,24 @@ export class Fuse {
     [key: string]: Contract;
   };
 
-  addresses: any;
-
   constructor(provider: providers.JsonRpcProvider, chainId: ChainID) {
     if (!isSupportedChainId(chainId)) {
       throw new Error(`Unsupported chainid: ${chainId}`);
     }
 
     this.provider = provider;
-    this.addresses = ADDRESSES[chainId];
-
-    console.log(this.addresses);
-
-    this.contracts = {
-      FusePoolDirectory: new Contract(
-        this.addresses.FUSE_POOL_DIRECTORY_CONTRACT_ADDRESS,
-        FUSE_CONTRACTS.FusePoolDirectoryABI,
-        this.provider
-      ),
-      FusePoolLens: new Contract(
-        this.addresses.FUSE_POOL_LENS_CONTRACT_ADDRESS,
-        FUSE_CONTRACTS.FusePoolLensABI,
-        this.provider
-      ),
-      FusePoolLensSecondary: new Contract(
-        this.addresses.FUSE_POOL_LENS_SECONDARY_CONTRACT_ADDRESS,
-        FUSE_CONTRACTS.FusePoolLensSecondaryABI,
-        this.provider
-      ),
-      FuseSafeLiquidator: new Contract(
-        this.addresses.FUSE_SAFE_LIQUIDATOR_CONTRACT_ADDRESS,
-        FUSE_CONTRACTS.FuseSafeLiquidatorABI,
-        this.provider
-      ),
-      FuseFeeDistributor: new Contract(
-        this.addresses.FUSE_FEE_DISTRIBUTOR_CONTRACT_ADDRESS,
-        FUSE_CONTRACTS.FuseFeeDistributorABI,
-        this.provider
-      ),
-    };
+    this.contracts = getContracts(provider, chainId);
   }
+
+  public getPools = async () => {
+    const [poolList, poolDescriptions] =
+      await this.contracts.FusePoolDirectory.functions.getPublicPoolsByVerification(
+        true
+      );
+
+    return {
+      poolList,
+      poolDescriptions,
+    };
+  };
 }
--- sdk/testbench.ts
+++ sdk/testbench.ts
@@ -5,49 +5,27 @@ import Joi from "joi";
 import { providers } from "ethers";
 
 // Utilities
-import { logger } from "../scripts/utilities/logger";
 import { validateEnvConfig } from "../scripts/utilities/validateConfig";
 
 // Fuse
 import { Fuse } from "./Fuse";
 
 // Validate values from .env file
+const { CHAIN_ID, ETH_RPC_URL } = validateEnvConfig(
+  "../.env",
+  Joi.object({
+    CHAIN_ID: Joi.number().default(1),
+    ETH_RPC_URL: Joi.string().default(""),
+  })
+);
 
 const main = async () => {
-  const { CHAIN_ID, ETH_RPC_URL } = validateEnvConfig(
-    "../.env",
-    Joi.object({
-      CHAIN_ID: Joi.number().default(1),
-      ETH_RPC_URL: Joi.string().default(""),
-    })
-  );
-
   const provider = new providers.JsonRpcProvider(ETH_RPC_URL);
   const fuse = new Fuse(provider, CHAIN_ID);
 
-  console.log(fuse);
+  const { poolList, poolDescriptions } = await fuse.getPools();
 
-  //   var pools = await fuse.contracts.FusePoolDirectory.methods
-  //     .getPublicPoolsByVerification(true)
-  //     .call();
-
-  //   var poolsList = pools[0];
-
-  //   for (let i = 0; i < poolsList.length; i++) {
-  //     var comptroller = new fuse.web3.eth.Contract(
-  //       JSON.parse(
-  //         fuse.compoundContracts["contracts/Comptroller.sol:Comptroller"].abi
-  //       ),
-  //       (
-  //         await fuse.contracts.FusePoolDirectory.methods
-  //           .pools(poolsList[i])
-  //           .call()
-  //       ).comptroller
-  //     );
-
-  //     console.log("pool number ", poolsList[i]);
-  //     console.log(await comptroller.methods.comptrollerImplementation().call());
-  //   }
+  console.log(poolDescriptions);
 };
 
 main().catch((error) => {
