2f2821833a18e552839707741a4381fabf7b0c26
cryptickoan <me@cryptickoan.com>
2022-06-21 17:49:09 -0600

    Fixed imports

--- scripts/abi/after-build/after-build.sh
+++ scripts/abi/after-build/after-build.sh
@@ -5,19 +5,19 @@ echo "${YELLOW}==========================================================="
 echo "4/4 Creating package.jsons"
 echo "===========================================================${NOCOLOR}"
 
-cat ./abi/scripts/after-build/cjs.json >> ./abi/packages/arbitrum/dist/cjs/package.json
-cat ./abi/scripts/after-build/cjs.json >> ./abi/packages/core/dist/cjs/package.json
-cat ./abi/scripts/after-build/cjs.json >> ./abi/packages/liquidators/dist/cjs/package.json
-cat ./abi/scripts/after-build/cjs.json >> ./abi/packages/oracles/dist/cjs/package.json
-cat ./abi/scripts/after-build/cjs.json >> ./abi/packages/periphery/dist/cjs/package.json
-cat ./abi/scripts/after-build/cjs.json >> ./abi/packages/utils/dist/cjs/package.json
+cat ./scripts/abi/after-build/cjs.json >> ./abi/packages/arbitrum/dist/cjs/package.json
+cat ./scripts/abi/after-build/cjs.json >> ./abi/packages/core/dist/cjs/package.json
+cat ./scripts/abi/after-build/cjs.json >> ./abi/packages/liquidators/dist/cjs/package.json
+cat ./scripts/abi/after-build/cjs.json >> ./abi/packages/oracles/dist/cjs/package.json
+cat ./scripts/abi/after-build/cjs.json >> ./abi/packages/periphery/dist/cjs/package.json
+cat ./scripts/abi/after-build/cjs.json >> ./abi/packages/utils/dist/cjs/package.json
 
-cat ./abi/scripts/after-build/esm.json >> ./abi/packages/arbitrum/dist/esm/package.json
-cat ./abi/scripts/after-build/esm.json >> ./abi/packages/core/dist/esm/package.json
-cat ./abi/scripts/after-build/esm.json >> ./abi/packages/liquidators/dist/esm/package.json
-cat ./abi/scripts/after-build/esm.json >> ./abi/packages/oracles/dist/esm/package.json
-cat ./abi/scripts/after-build/esm.json >> ./abi/packages/periphery/dist/esm/package.json
-cat ./abi/scripts/after-build/esm.json >> ./abi/packages/utils/dist/esm/package.json
+cat ./scripts/abi/after-build/esm.json >> ./abi/packages/arbitrum/dist/esm/package.json
+cat ./scripts/abi/after-build/esm.json >> ./abi/packages/core/dist/esm/package.json
+cat ./scripts/abi/after-build/esm.json >> ./abi/packages/liquidators/dist/esm/package.json
+cat ./scripts/abi/after-build/esm.json >> ./abi/packages/oracles/dist/esm/package.json
+cat ./scripts/abi/after-build/esm.json >> ./abi/packages/periphery/dist/esm/package.json
+cat ./scripts/abi/after-build/esm.json >> ./abi/packages/utils/dist/esm/package.json
 
 echo "${GREEN}==========================================================="
 echo "Packages created successfully!"
--- scripts/abi/tsconfig/tsconfig.sh
+++ scripts/abi/tsconfig/tsconfig.sh
@@ -9,23 +9,23 @@ rm ./abi/packages/**/tsconfig-base.json
 rm ./abi/packages/**/tsconfig-esm.json
 rm ./abi/packages/**/tsconfig-cjs.json
 
-cat ./abi/scripts/tsconfig/tsconfig-base.json >> ./abi/packages/arbitrum/tsconfig-base.json
-cat ./abi/scripts/tsconfig/tsconfig-base.json >> ./abi/packages/core/tsconfig-base.json
-cat ./abi/scripts/tsconfig/tsconfig-base.json >> ./abi/packages/liquidators/tsconfig-base.json
-cat ./abi/scripts/tsconfig/tsconfig-base.json >> ./abi/packages/oracles/tsconfig-base.json
-cat ./abi/scripts/tsconfig/tsconfig-base.json >> ./abi/packages/periphery/tsconfig-base.json
-cat ./abi/scripts/tsconfig/tsconfig-base.json >> ./abi/packages/utils/tsconfig-base.json
+cat ./scripts/abi/tsconfig/tsconfig-base.json >> ./abi/packages/arbitrum/tsconfig-base.json
+cat ./scripts/abi/tsconfig/tsconfig-base.json >> ./abi/packages/core/tsconfig-base.json
+cat ./scripts/abi/tsconfig/tsconfig-base.json >> ./abi/packages/liquidators/tsconfig-base.json
+cat ./scripts/abi/tsconfig/tsconfig-base.json >> ./abi/packages/oracles/tsconfig-base.json
+cat ./scripts/abi/tsconfig/tsconfig-base.json >> ./abi/packages/periphery/tsconfig-base.json
+cat ./scripts/abi/tsconfig/tsconfig-base.json >> ./abi/packages/utils/tsconfig-base.json
 
-cat ./abi/scripts/tsconfig/tsconfig-esm.json >> ./abi/packages/arbitrum/tsconfig-esm.json
-cat ./abi/scripts/tsconfig/tsconfig-esm.json >> ./abi/packages/core/tsconfig-esm.json
-cat ./abi/scripts/tsconfig/tsconfig-esm.json >> ./abi/packages/liquidators/tsconfig-esm.json
-cat ./abi/scripts/tsconfig/tsconfig-esm.json >> ./abi/packages/oracles/tsconfig-esm.json
-cat ./abi/scripts/tsconfig/tsconfig-esm.json >> ./abi/packages/periphery/tsconfig-esm.json
-cat ./abi/scripts/tsconfig/tsconfig-esm.json >> ./abi/packages/utils/tsconfig-esm.json
+cat ./scripts/abi/tsconfig/tsconfig-esm.json >> ./abi/packages/arbitrum/tsconfig-esm.json
+cat ./scripts/abi/tsconfig/tsconfig-esm.json >> ./abi/packages/core/tsconfig-esm.json
+cat ./scripts/abi/tsconfig/tsconfig-esm.json >> ./abi/packages/liquidators/tsconfig-esm.json
+cat ./scripts/abi/tsconfig/tsconfig-esm.json >> ./abi/packages/oracles/tsconfig-esm.json
+cat ./scripts/abi/tsconfig/tsconfig-esm.json >> ./abi/packages/periphery/tsconfig-esm.json
+cat ./scripts/abi/tsconfig/tsconfig-esm.json >> ./abi/packages/utils/tsconfig-esm.json
 
-cat ./abi/scripts/tsconfig/tsconfig-cjs.json >> ./abi/packages/arbitrum/tsconfig-cjs.json
-cat ./abi/scripts/tsconfig/tsconfig-cjs.json >> ./abi/packages/core/tsconfig-cjs.json
-cat ./abi/scripts/tsconfig/tsconfig-cjs.json >> ./abi/packages/liquidators/tsconfig-cjs.json
-cat ./abi/scripts/tsconfig/tsconfig-cjs.json >> ./abi/packages/oracles/tsconfig-cjs.json
-cat ./abi/scripts/tsconfig/tsconfig-cjs.json >> ./abi/packages/periphery/tsconfig-cjs.json
-cat ./abi/scripts/tsconfig/tsconfig-cjs.json >> ./abi/packages/utils/tsconfig-cjs.json
+cat ./scripts/abi/tsconfig/tsconfig-cjs.json >> ./abi/packages/arbitrum/tsconfig-cjs.json
+cat ./scripts/abi/tsconfig/tsconfig-cjs.json >> ./abi/packages/core/tsconfig-cjs.json
+cat ./scripts/abi/tsconfig/tsconfig-cjs.json >> ./abi/packages/liquidators/tsconfig-cjs.json
+cat ./scripts/abi/tsconfig/tsconfig-cjs.json >> ./abi/packages/oracles/tsconfig-cjs.json
+cat ./scripts/abi/tsconfig/tsconfig-cjs.json >> ./abi/packages/periphery/tsconfig-cjs.json
+cat ./scripts/abi/tsconfig/tsconfig-cjs.json >> ./abi/packages/utils/tsconfig-cjs.json
